f7b5b029a5de803be5d9eea16e74e9e27c5474ec
PieTurri <piero.turri@stud.unifi.it>
2018-09-27 10:15:54 +0200

    SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Ciao ragazzi ben tornati! Guardiamo se so programmare" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(1400,900), "SFML work!!");
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Green);
+    }
+
+    window.clear();
+    window.display();
 }
